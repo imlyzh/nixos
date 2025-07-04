396b292b907d7710d416d7cc8a43af8c0a341b93
imlyzh <enterindex@gmail.com>
2025-07-04 11:36:34 +0800

    update

--- machines/darwin-configuration.nix
+++ machines/darwin-configuration.nix
@@ -24,13 +24,26 @@
     options = "--delete-older-than 30d";
   };
   system.defaults = {
+    NSGlobalDomain.AppleShowAllExtensions = true;
+    NSGlobalDomain.AppleShowAllFiles = true;
+    NSGlobalDomain.NSDocumentSaveNewDocumentsToCloud = false;
+    NSGlobalDomain.NSUseAnimatedFocusRing = false;
+    ActivityMonitor.IconType = 0;
+    WindowManager = {
+      EnableTiledWindowMargins = false;
+      GloballyEnabled = true;
+    };
+    controlcenter = {
+      BatteryShowPercentage = true;
+      wvous-bl-corner = 7;
+    };
     finder = {
       AppleShowAllFiles = true;
       _FXShowPosixPathInTitle = true; # 在标题栏显示完整路径
       AppleShowAllExtensions = true;   # 显示所有文件扩展名
       FXDefaultSearchScope = "SCcf";
       NewWindowTarget = "Home";
-    #   QuitMenuItem = true;
+      QuitMenuItem = false;
       ShowMountedServersOnDesktop = true;
       ShowPathbar = true;
       ShowStatusBar = false;
@@ -39,8 +52,12 @@
     };
     dock = {
       autohide = true;
-      show-recents = false;
+      show-recents = true;
       mru-spaces = false;
+      appswitcher-all-displays = true;
+      expose-group-apps = true;
+      scroll-to-open = true;
+      wvous-bl-corner = 1;
     };
   };
 #   environment.variables = {
