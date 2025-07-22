24643a2871da507aa86695765cafebe170d0226e
imlyzh <enterindex@gmail.com>
2025-07-23 07:01:16 +0800

    Refactor NixOS config: switch to Sway with greetd login manager
    - Replace Hyprland/GDM with Sway and greetd/wlgreet for lighter setup
    - Clean up commented code and simplify configuration
    - Keep Chinese locale and Fcitx5 input method settings
    - Add basic Sway keybindings for usability
    - Maintain experimental Nix features (flakes, nix-command)
    - Preserve essential system settings (timezone, filesystems, etc.)

--- machines/configuration.nix
+++ machines/configuration.nix
@@ -1,6 +1,5 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+# /etc/nixos/configuration.nix
+# 小狐娘帮你改造好啦！
 
 { config, lib, pkgs, inputs,... }:
 {
@@ -19,39 +18,21 @@
   nix = {
     settings = {
       substituters = [
-        # "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-        # "https://mirrors.ustc.edu.cn/nix-channels/store"
-        # "https://mirrors.sjtug.sjtu.edu.cn/nix-channels/store"
         "https://cache.nixos.org/"
       ];
     };
+    settings.experimental-features = [ "nix-command" "flakes" ];
   };
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
   boot.supportedFilesystems = [ "btrfs" ];
-  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  # networking.hostName = "lyzh-nixos"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-
-  # Set your time zone.
   time.timeZone = "Asia/Shanghai";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://localhost:20171";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8"; # «--- 小狐娘建议开启这个，对很多程序有好处
-  i18n.extraLocaleSettings = { # «--- 小狐娘帮你把中文环境也配好啦
+  i18n.defaultLocale = "zh_CN.UTF-8";
+  i18n.extraLocaleSettings = {
     LC_ADDRESS = "zh_CN.UTF-8";
     LC_IDENTIFICATION = "zh_CN.UTF-8";
     LC_MEASUREMENT = "zh_CN.UTF-8";
@@ -64,64 +45,106 @@
   };
 
   i18n.inputMethod = {
-    # 这是根据警告信息修改的，更现代的写法！
     enable = true;
     type = "fcitx5";
-    # 我们把插件列表写得更精确，就不会找不到了！
-    fcitx5.addons = [
-      pkgs.fcitx5-chinese-addons
-      pkgs.fcitx5-gtk
-      # pkgs.fcitx5-qt  # «--- 看！我们告诉了它完整的路径 pkgs.fcitx5-qt！
+    fcitx5.addons = with pkgs; [
+      fcitx5-chinese-addons
+      fcitx5-gtk
+      fcitx5-qt
     ];
   };
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  # --- 小狐娘在这里做了大改造哦！ ---
+  # 1. 我们不再需要 Xorg 服务和 GDM 啦，Sway 会自己处理好一切
+  # services.xserver.enable = true; # 注释掉
+  # services.displayManager.gdm.enable = true; # 注释掉
+  # programs.hyprland.enable = true; # 把 Hyprland 也收起来
 
-  # «--- 小狐娘在这里做了改动哦！ ---»
-  # 我们保留 GDM 作为登录器，但禁用完整的 GNOME 桌面，这样更轻量
-  services.displayManager.gdm.enable = true;
-  # services.desktopManager.gnome.enable = true; # «--- 注释掉这个！
-
-  # «--- 小狐娘在这里添加了 Hyprland 的核心配置 ---»
-  programs.hyprland = {
+  # 2. 换上轻巧漂亮的 greetd 登录管理器！
+  services.greetd = {
     enable = true;
-    xwayland.enable = true;
+    # wlgreet 是 greetd 的 Wayland 界面，很搭哦
+    settings.default_session.command = "${pkgs.greetd.wlgreet}/bin/wlgreet --command sway";
+    # 如果你想默认启动 Niri，就把上面的 sway 换成 niri
   };
 
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+  # 3. 这是 Sway 的魔法配置区！(当前启用)
+  programs.sway = {
+    enable = true;
+    # 如果主人想要带特效的 SwayFX，就把下面这行的注释去掉，再把上一行注释掉！
+    # package = pkgs.swayfx;
+
+    # 小狐娘给你准备了一份基础配置，这样开机就能用啦！
+    extraConfig = ''
+      # 设置你的 $mod 键，Win 键就是 Mod4
+      set $mod Mod4
+
+      # 启动终端 (主人装了 kitty，我们就用 kitty)
+      bindsym $mod+Return exec kitty
+
+      # 关闭窗口
+      bindsym $mod+Shift+q kill
+
+      # 启动程序 (用 wofi)
+      bindsym $mod+d exec wofi --show drun
+
+      # 锁屏
+      bindsym $mod+Control+l exec swaylock
+
+      # 重载配置
+      bindsym $mod+Shift+c reload
+
+      # 退出 Sway
+      bindsym $mod+Shift+e exec swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' -B 'Yes, exit sway' 'swaymsg exit'
+    '';
+
+    # 让 Sway 也能用 X11 的老程序
+    xwayland = true;
+  };
+
+  # 4. 这是 Niri 的设计图纸！(已注释，随时可以启用)
+  /*
+  programs.niri = {
+    enable = true;
+    # 小狐娘也给 Niri 准备了基础配置
+    settings = {
+      "preference" = {
+        terminal = "${pkgs.kitty}/bin/kitty";
+      };
+      "keybinding" = [
+        {
+          keys = [ "Super" "d" ];
+          command = [ "wofi" "--show" "drun" ];
+        }
+      ];
+    };
+  };
+  */
+
+  # 5. Wayland 世界的“胶水”程序，非常重要！
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-wlr ];
+    # 如果发现截图或文件选择有问题，可以把下面这个也打开
+    # extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+
   services.xserver.xkb.options = "caps:escape";
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # «--- 小狐娘帮你启用了 Pipewire 来管理声音 ---»
-  # services.pulseaudio.enable = true; # 这个是旧的，我们不用啦
   services.pipewire = {
     enable = true;
     pulse.enable = true;
-    alsa.enable = true;    # 让老程序也能用
+    alsa.enable = true;
     alsa.support32Bit = true;
-    jack.enable = true;    # 专业音频支持
+    jack.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
 
   services.tailscale.enable = true;
   services.v2raya.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.lyzh = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" ];
     uid = 1001;
     hashedPassword = "$6$3EPkfBlo6DmngTcl$fxPkkvpjjSyAniQoZ2roAGCvgKXG51e824SDEr3FtMXX.E4h3qIxsNMLI6d0KZeAvLQrtgUkbu4m1dLeYJ11H.";
     packages = with pkgs; [];
@@ -129,8 +152,6 @@
 
   programs.firefox.enable = true;
 
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
     btrfs-progs
     vim
@@ -144,18 +165,18 @@
     v2raya
     proxychains-ng
 
-    # «--- 小狐娘在这里加上了 Hyprland 的好朋友们！ ---»
-    waybar       # 漂亮的状态栏
-    kitty        # 一个速度很快的终端
-    wofi         # 应用启动器 (可以换成 rofi-wayland)
-    mako         # 通知弹窗
-    swww         # 设置壁纸的小可爱
-    swaylock     # 锁屏工具
-    grim         # 截图工具
-    slurp        # 截图时用来选择区域的
-    wl-clipboard # Wayland 的剪贴板工具
+    # Hyprland 的朋友们很多和 Sway 是通用的！
+    waybar
+    kitty
+    wofi
+    mako
+    swww # Sway 可以用 swaybg，但 swww 也很棒！
+    swaylock # 锁屏工具
+    swayidle # 空闲管理，可以配合锁屏用
+    grim
+    slurp
+    wl-clipboard
 
-    # «--- 小狐娘贴心推荐的输入法 ---»
     fcitx5
   ];
 
@@ -169,7 +190,7 @@
     CC = "clang";
     CXX = "clang++";
 
-    # «--- 小狐娘在这里添加了输入法需要的环境变量 ---»
+    # 输入法环境变量，非常重要！
     GTK_IM_MODULE = "fcitx";
     QT_IM_MODULE = "fcitx";
     XMODIFIERS = "@im=fcitx";
@@ -180,14 +201,8 @@
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
     stdenv.cc.cc.lib
-    # any other system-level libraries vscode-server might need
   ];
 
-  #programs.vscode.server.enable = true;
-
-  #environment.sessionVariables = { ... }; # 主人这里原来是注释掉的，小狐娘就没动它
-
-  # List services that you want to enable:
   services.openssh.enable = true;
   services.samba = {
     enable = true;
@@ -200,7 +215,6 @@
         "read only" = "yes";
       };
     };
-    # openFirewall = true;
   };
   services.nfs.server = {
     enable = true;
@@ -208,5 +222,5 @@
     hostName = "lyzh-nixos";
   };
   networking.firewall.enable = false;
-  system.stateVersion = "25.05"; # Did you read the comment?
+  system.stateVersion = "25.05";
 }
