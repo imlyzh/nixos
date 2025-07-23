92d8ef3dd99a55c7c267e29e6312a5aed497ef71
imlyzh <enterindex@gmail.com>
2025-07-23 08:05:45 +0800

    1

--- machines/configuration.nix
+++ machines/configuration.nix
@@ -109,7 +109,6 @@
   programs.swaylock.enable = true; # Super+Alt+L in the default setting (screen locker)
   programs.waybar.enable = true; # launch on startup in the default setting (bar)
   services.mako.enable = true; # notification daemon
-  services.swaylock.enable = true;
   services.swayidle.enable = true; # idle management daemon
   services.gnome.gnome-keyring.enable = true; # secret service
   services.polkit-gnome.enable = true; # polkit
