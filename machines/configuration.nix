d6da4458d3fec8e390432567dba9b7d8735a731f
imlyzh <enterindex@gmail.com>
2025-07-23 08:05:15 +0800

    1

--- machines/configuration.nix
+++ machines/configuration.nix
@@ -108,7 +108,6 @@
   # programs.fuzzel.enable = true; # Super+D in the default setting (app launcher)
   programs.swaylock.enable = true; # Super+Alt+L in the default setting (screen locker)
   programs.waybar.enable = true; # launch on startup in the default setting (bar)
-  programs.swaybg.enable = true;
   services.mako.enable = true; # notification daemon
   services.swaylock.enable = true;
   services.swayidle.enable = true; # idle management daemon
