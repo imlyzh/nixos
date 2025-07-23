1669f1a6568266bda8ec41aa9b216dfcd789f5c6
imlyzh <enterindex@gmail.com>
2025-07-23 08:38:08 +0800

    11

--- machines/hardware-configuration.nix
+++ machines/hardware-configuration.nix
@@ -55,6 +55,7 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
+  networking.networkmanager.enable = true;
   # networking.interfaces.enp2s0f0.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
